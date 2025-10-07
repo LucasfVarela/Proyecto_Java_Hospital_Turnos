141db1459b430baba0443621c198a530f2d77408
lautaronuccitelli <krebz234@gmail.com>
2025-10-07 04:54:47 -0300

    model de medico agregado

--- src/Model/Medico.java
+++ src/Model/Medico.java
@@ -4,10 +4,78 @@
  */
 package Model;
 
+import java.time.LocalTime;
+
 /**
  *
- * @author lautisape
+ * @author lautaro
  */
 public class Medico {
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private String especialidad;
+    private String telefono;
+    private LocalTime horarios;
     
+    public Medico(){
+    }
+    
+    public Medico(Integer id, String nombre, String apellido, String especialidad, String telefono, LocalTime horarios){
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.especialidad = especialidad;
+        this.telefono = telefono;
+        this.horarios = horarios;
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    public String getEspecialidad(){
+        return especialidad;
+    }
+    
+    public void setEsPecialidad(String especialidad){
+        this.especialidad = especialidad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+
+    public LocalTime getHorarios() {
+        return horarios;
+    }
+
+    public void setHorarios(LocalTime horarios) {
+        this.horarios = horarios;
+    }
 }
--- src/Model/Paciente.java
+++ src/Model/Paciente.java
@@ -4,10 +4,109 @@
  */
 package Model;
 
+import java.time.LocalDate;
+
 /**
  *
- * @author lautisape
+ * @author lautaro
  */
 public class Paciente {
     
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private String numeroDocumento;
+    private String telefono;
+    private String email;
+    private LocalDate fechaNacimiento;
+
+    
+    public Paciente() {
+    }
+    
+    public Paciente(Integer id, String nombre, String apellido, String numeroDocumento, 
+                   String telefono, String email, LocalDate fechaNacimiento) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numeroDocumento = numeroDocumento;
+        this.telefono = telefono;
+        this.email = email;
+        this.fechaNacimiento = fechaNacimiento;
+
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+
+
+    public String getNombreCompleto() {
+        return apellido + ", " + nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", numeroDocumento='" + numeroDocumento + '\'' +
+                ", telefono='" + telefono + '\'' +
+                '}';
+    }
 }
