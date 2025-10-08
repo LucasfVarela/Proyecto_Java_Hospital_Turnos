0b286e27197e567131068efd48977f4feab4583d
lautaronuccitelli <krebz234@gmail.com>
2025-10-08 03:43:33 -0300

    turno

---
+++ src/Model/Turno.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author lautisape
+ */
+public class Turno {
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private String numeroDocumento;
+    private String telefono;
+    private String especialidad;
+    private String Observaciones;
+    
+    public Turno(){
+        
+    }
+    
+    public Turno(Integer id, String nombre, String apellido, String numeroDocumento, String telefono, String especialdad, String observaciones){
+        
+    }
+
+}
