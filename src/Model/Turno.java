12c01e13dbc6e0ebd261db92607960749a709062
lautaronuccitelli <krebz234@gmail.com>
2025-10-08 03:51:31 -0300

    turno terminado parcialmente

--- src/Model/Turno.java
+++ src/Model/Turno.java
@@ -15,14 +15,88 @@ public class Turno {
     private String numeroDocumento;
     private String telefono;
     private String especialidad;
-    private String Observaciones;
+    private String observaciones;
     
     public Turno(){
         
     }
     
-    public Turno(Integer id, String nombre, String apellido, String numeroDocumento, String telefono, String especialdad, String observaciones){
-        
+    public Turno(Integer id, String nombre, String apellido, String numeroDocumento, String telefono, String especialidad, String observaciones){
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numeroDocumento = numeroDocumento;
+        this.telefono = telefono;
+        this.especialidad = especialidad;
+        this.observaciones = observaciones;
+    }
+    
+        public Integer getId() {
+        return id;
     }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+    
+        @Override
+    public String toString() {
+        return "Turno{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", numeroDocumento='" + numeroDocumento + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", especialidad='" + especialidad + '\'' +
+                ", observaciones='" + observaciones + '\'' +
+                '}';
+    }
 }
